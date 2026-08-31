21f0edd2f8f4bed1076e76b0ef918c731dc9d6c6
rahulnallamothu313-hub <rahulnallamothu313@gmail.com>
2026-08-31 15:54:52 +0530

    comparision operators exercise examples

---
+++ comparision operator.py
@@ -0,0 +1,16 @@
+# python comparision operator exercise
+A=20
+B=60
+result = A > B #Greater than comparision
+print("result of",A, ">",B,"is :",result)
+result = A < B #Less than comparision
+print("result of",A, "<",B,"is :",result)
+result = A != B #Not equal to comparision
+print("result of",A, "!=",B, "is :",result)
+result = A == B # Equal to comparision
+print("result of ",A, "==",B, "is :",result)
+result = A >= B #Greater than or equal to comparision
+print("result of ",A, ">=",B, "is :",result)
+result = A <= B # less than or equal to comparision
+print("result of ",A, "<=",B, "is :",result)
+
