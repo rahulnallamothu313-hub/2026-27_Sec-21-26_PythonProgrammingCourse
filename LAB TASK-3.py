962d11b78188c6fe8a385c93db97803885e41ed6
rahulnallamothu313-hub <rahulnallamothu313@gmail.com>
2026-09-03 21:24:42 +0530

    Lab task-3(Area and perimeter of the rectangle)

---
+++ LAB TASK-3.py
@@ -0,0 +1,7 @@
+#AREA AND PERIMETER OF THE RECTANGLE
+length=int(input("Enter length:"))
+breadth=int(input("Enter breadth:"))
+area=length * breadth
+perimeter=2 * length+breadth
+print("area:",area)
+print("perimeter:",perimeter)
