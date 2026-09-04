b0f058d0096edbff4d2b0f14140acfb2d8184888
rahulnallamothu313-hub <rahulnallamothu313@gmail.com>
2026-09-04 14:21:57 +0530

    identity operators exercise examples.

---
+++ identity operators.py
@@ -0,0 +1,17 @@
+#identity operators.
+a=[1,2,3,4]
+b=a
+result= a is b
+print("result of ",a, "is ",b, "is :",result)
+a=[1,2,3,4]
+b=[1,2,3,4]
+result= a is b
+print("result of ",a, "is",b, "is :",result)
+a=[1,2,3,4]
+b=a 
+result=a is not b
+print("result of ",a, "is not",b, "is :",result)
+a=[1,2,3,4]
+b=[1,2,3,4]
+result = a is not b
+print("result of ",a, "is not",b, "is :",result)
