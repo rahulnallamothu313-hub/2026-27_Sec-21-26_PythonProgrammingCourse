749764eaeacdd43f39275f215cd9b1becf091e0c
rahulnallamothu313-hub <rahulnallamothu313@gmail.com>
2026-09-07 20:01:09 +0530

    arithmetic operators exercise examples.

---
+++ arithematic.py
@@ -0,0 +1,22 @@
+#arithmetic operators
+num1=int(input("Enter the value of num1: "))
+num2=int(input("enter the value of num2:"))
+sum=num1+num2 #addition (+)
+print("sum of the two numbers:",sum)
+
+difference=num1-num2 #subtraction (-)
+print("difference between two numbers:",difference)
+
+product=num1*num2 #multiplication (*)
+print("product between two numbers:",product)
+
+division=num1/num2 #division (/)
+print("division between two numbers:",division)
+
+modulus=num1%num2 #modulus(%)
+print("modulus between two numbers:",modulus)
+floordivision=num1//num2 #floordivision(//)
+print("floordivision between two numbers:",floordivision)
+
+exponentiation=num1**num2 #exponentiation (**)
+print("exponentiation between two numbers:",exponentiation)
