bf1f9c4d6ddf5eef75c8e6853246bd44acd9798a
rahulnallamothu313-hub <rahulnallamothu313@gmail.com>
2026-09-07 20:09:10 +0530

    lab task-5(swapping the two numbers)

--- LAB TASK-5.py
+++ LAB TASK-5.py
@@ -1,4 +1,4 @@
-A=int(input("Enter the value of A:"))
-B=int(input("Enter the value of B:"))
-A,B = B,A
-print(A,B)
+num1=int(input("Enter the value of num1:"))
+num2=int(input("Enter the value of num2:"))
+swap=num1,num2=num2,num1
+print("swap of num1,num2:",swap)
